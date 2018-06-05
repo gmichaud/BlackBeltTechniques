e913f0f33e8cf8d938251748fae12e7ca9da5e0b
Gabriel Michaud <gabriel@velixo.com>
2018-06-05 10:59:25 -0400

    Initial commit

---
+++ Velixo.BlackBeltTechniques/GlobalGraphExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using PX.Data;
+
+namespace Velixo.BlackBeltTechniques
+{
+    public class GlobalGraphExtension : PXGraphExtension<PXGraph>
+    {
+        public override void Initialize()
+        {
+            if (!String.IsNullOrEmpty(Base.PrimaryView))
+            { 
+                Type primaryViewItemType = Base.Views[Base.PrimaryView].Cache.GetItemType();
+                PXAction action = PXNamedAction.AddAction(Base, primaryViewItemType, "Test", "Test", TestClick);
+            }
+        }
+
+        public IEnumerable TestClick(PXAdapter adapter)
+        {
+            throw new PXException("Test button clicked from graph " + Base.GetType().Name);
+        }
+    }
+}
